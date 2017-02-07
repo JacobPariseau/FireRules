5471485faf0de2b7544a9ab69f28b75609c9aa52
Jɑcob Pɑriseɑu <jacob@jacobpariseau.com>
2017-02-07 11:53:06 -0700

    Local database and random selection

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
-$('#die').on('click', function (e) {
-  const die = $(this);
+/** Database file */
+const DB = $.getJSON('db.json', function (data) {
+  console.log(data);
+  return data;
+});
+
+/** Element constants */
+const die = $('#die');
+const rulePanel = $('#rule');
+const ruleTitle = $('#rule-title');
+const ruleBody = $('#rule-body');
+
+die.on('click', function (e) {
   die.addClass('spin');
 
   _.delay(function () {
-    $('#rule').addClass('show');
+    rulePanel.addClass('show');
   }, 600);
 
   _.delay(function () {
     die.removeClass('spin');
   }, 1000);
+
+  let rule = _.sample(DB);
+  ruleTitle.text(rule.title);
+  ruleBody.text(rule.body);
+
 });
 
 $(document).click(function() {
